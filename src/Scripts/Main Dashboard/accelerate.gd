1f69ac817ad0d403f41c0f6d4085e40424420275
Maureen Gloria <maureenmgloria@gmail.com>
2021-04-13 02:06:57 -0500

    Add speedometer with accelerator button

---
+++ src/Scripts/Main Dashboard/accelerate.gd
@@ -0,0 +1,50 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocity = 0
+var acceleration = .3
+var elapsed = 0
+var distance = 0
+const MAX_SPEED = 270
+var speed = 0
+
+var accelerating = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(speed)
+	if(Input.is_mouse_button_pressed(BUTTON_LEFT) and accelerating):
+		if(speed < MAX_SPEED):
+			elapsed += delta
+			velocity = velocity + (elapsed * acceleration)
+			distance = velocity * elapsed + (.5 * (acceleration*(elapsed*elapsed)))
+			speed = distance/elapsed
+	elif(speed > 0):
+		speed -= 1
+		
+	
+	get_tree().get_root().find_node("MPH_Label",true,false).text = "mph\n" + str(round(floor(abs(speed))))
+	var dial = get_node(@"../../placeholdercircle/Parent_Dial/Dial")
+	# print(4.5 * speed/280)
+#	dial.rotate((4.5 * speed/280) - dial.rotation)
+	dial.rotation_degrees = speed
+	# print(dial.rotation)
+
+
+func _on_Button_button_up():
+	# elapsed = 0
+	accelerating = false
+	# Replace with function body.
+
+
+func _on_Button_button_down():
+	accelerating = true
+	# pass # Replace with function body.
--- src/Test_Static_Body.gd
+++ src/Test_Static_Body.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends StaticBody2D
 
 
 # Declare member variables here. Examples:
@@ -8,13 +8,11 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _on_Area2D_mouse_entered():
-	self.rotate(.01)
-	print('aaaaaaaaaaaaaa')
+func _process(delta):
+	# self.rotate(.002)
+	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		self.rotate(.002)
