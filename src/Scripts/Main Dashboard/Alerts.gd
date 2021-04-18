c407cc10b4e50496667a0f02dc70f144b1bd3de7
Maureen Gloria <maureenmgloria@gmail.com>
2021-04-17 19:47:27 -0500

    Connect emergency and add alerts

---
+++ src/Scripts/Main Dashboard/Alerts.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Tire_Pressure_pressed():
+	$"tire-pressure".visible = !$"tire-pressure".visible
+
+
+func _on_Show_Alerts_pressed():
+	for n in self.get_children():
+		print('weeh')
+
+
+func _on_Tire_Pressure_toggled(button_pressed):
+	if(button_pressed):
+		$"tire-pressure".visible = false
+	else:
+		$"tire-pressure".visible = true
+	
+	emit_signal('tire_pressure_toggled')
+	# pass # Replace with function body.
+
+
+func _on_Show_Alerts_toggled(button_pressed):
+	if(button_pressed):
+		for n in self.get_children():
+			n.visible = false
+	else:
+		for n in self.get_children():
+			n.visible = true
+		$"../Control/Tire Pressure".pressed = false
+			#$"../Control/Tire Pressure"._toggled = false
+		
--- src/Scripts/dashboard.gd
+++ src/Scripts/dashboard.gd
@@ -11,13 +11,14 @@ func _ready():
 	$Center.connect("emergency", self, "_on_emergency_pressed")
 	$Main/Control/Accelerate.connect("speed", self, "_a")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_emergency_pressed(button):
+	$Main/Control._on_Emergency_pressed()
 	print("Emergency: ", button)
 	
 func _a():
+	print($Center/Air_Flow)
 	print("too fast")
