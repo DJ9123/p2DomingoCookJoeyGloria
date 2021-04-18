94c1d6abe4bbb374c2940bf2e715c3ac3d7c6c29
DJ <dj9123@live.com>
2021-04-18 02:23:28 -0500

    Add base music screen and update radio screen

---
+++ src/Scripts/Center Dashboard/Music_Menu.gd
@@ -0,0 +1,199 @@
+extends Node2D
+
+#{
+#		"filename": "",
+#		"title": "",
+#		"artist": "",
+#		"album": "",
+#		"lighter": "",
+#		"darker": "",
+#	},
+
+var songs = [
+	{
+		"filename": "agony.png",
+		"title": "Agony",
+		"artist": "Yung Lean",
+		"album": "Stranger",
+		"lighter": "632c24",
+		"darker": "4e0001",
+	}, {
+		"filename": "the-chicken-and-the-egg.png",
+		"title": "The Chicken And The Egg",
+		"artist": "Takeo Ischi",
+		"album": "",
+		"lighter": "df8a83",
+		"darker": "cd4655",
+	}, {
+		"filename": "hit-my-line.png",
+		"title": "Hit My Line",
+		"artist": "Logic",
+		"album": "No Pressure",
+		"lighter": "d3af54",
+		"darker": "ea7212",
+	}, {
+		"filename": "astrid.png",
+		"title": "astrid",
+		"artist": "Glaive",
+		"album": "cypress grove",
+		"lighter": "73706A",
+		"darker": "0d5f46",
+	}, {
+		"filename": "soft-lipa-1.png",
+		"title": "When I Arrive",
+		"artist": "Soft Lipa",
+		"album": "Secrets of Tu Cheng Hsi: Renovate",
+		"lighter": "5d5084",
+		"darker": "141917",
+	}, {
+		"filename": "less-go.png",
+		"title": "LESS GO",
+		"artist": "DaBaby Car",
+		"album": "HUH",
+		"lighter": "aca484",
+		"darker": "595a52",
+	}, {
+		"filename": "nobody.png",
+		"title": "Nobody",
+		"artist": "Mitski",
+		"album": "Be the Cowboy",
+		"lighter": "928B91",
+		"darker": "243c3e",
+	}, {
+		"filename": "eugene.png",
+		"title": "Eugene",
+		"artist": "Sufjan Stevens",
+		"album": "Carrie & Lowell",
+		"lighter": "668288",
+		"darker": "3c342d",
+	}, {
+		"filename": "sayonara-wild-heart.png",
+		"title": "Sayonara Wild Heart",
+		"artist": "Daniel Olsén",
+		"album": "Sayonara Wild Hearts",
+		"lighter": "ae70ff",
+		"darker": "3173d3",
+	}, {
+		"filename": "shrike.png",
+		"title": "Shrike",
+		"artist": "Hozier",
+		"album": "Wasteland, Baby!",
+		"lighter": "8B8F8B",
+		"darker": "58a6a2",
+	}, {
+		"filename": "lullaby-in-the-peaceful-light.png",
+		"title": "Lullaby in the Peaceful Light",
+		"artist": "Masakatsu Takagi",
+		"album": "Wolf Children",
+		"lighter": "8CADB3",
+		"darker": "3b613b",
+	}, {
+		"filename": "fishy-on-me.png",
+		"title": "Fishy On Me",
+		"artist": "Tiko",
+		"album": "Certified Bangers",
+		"lighter": "9F9A9A",
+		"darker": "f2c26e",
+	}, 
+]
+
+var spriteTextures = [
+	preload("res://Sprites/Center Dashboard/Music/agony.png"),
+	preload("res://Sprites/Center Dashboard/Music/the-chicken-and-the-egg.png"),
+	preload("res://Sprites/Center Dashboard/Music/hit-my-line.png"),
+	preload("res://Sprites/Center Dashboard/Music/astrid.png"),
+	preload("res://Sprites/Center Dashboard/Music/soft-lipa-1.png"),
+	preload("res://Sprites/Center Dashboard/Music/less-go.png"),
+	preload("res://Sprites/Center Dashboard/Music/nobody.png"),
+	preload("res://Sprites/Center Dashboard/Music/eugene.png"),
+	preload("res://Sprites/Center Dashboard/Music/sayonara-wild-heart.png"),
+	preload("res://Sprites/Center Dashboard/Music/shrike.png"),
+	preload("res://Sprites/Center Dashboard/Music/lullaby-in-the-peaceful-light.png"),
+	preload("res://Sprites/Center Dashboard/Music/fishy-on-me.png"),
+]
+
+var nowPlayingLength = 150
+var nowPlayingIndex = 0
+var nowPlayingList = []
+var nowPlaying = {}
+
+var elapsed = 0
+var paused = false
+
+onready var lighter = $Lighter
+onready var darker = $Darker
+onready var sprite = $Sprite
+onready var progress = $TextureProgress
+onready var album = $Info/Album
+onready var title = $Info/Title
+onready var artist = $Info/Artist
+onready var pauseButton = $Controls/Pause/CenterContainer/Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	nowPlayingList = Array(songs)
+	nowPlaying = nowPlayingList[nowPlayingIndex]
+	updateNowPlaying()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not paused:
+		elapsed += delta
+		progress.value = elapsed / nowPlayingLength * 100
+		if progress.value >= 100:
+			_on_Next_activated(null)
+			
+func update_progress():
+	progress.value = elapsed / nowPlayingLength * 100
+	if progress.value >= 100:
+		_on_Next_activated(null)
+
+
+func updateNowPlaying():
+	print("nowPlaying: ", nowPlaying)
+	print(Color(nowPlaying.lighter))
+	print(Color(nowPlaying.darker))
+	elapsed = 0
+	paused = false
+	lighter.color = Color(nowPlaying.lighter)
+	darker.color = Color(nowPlaying.darker)
+	sprite.texture = spriteTextures[nowPlayingIndex % nowPlayingList.size()]
+	album.text = nowPlaying.album
+	title.text = nowPlaying.title
+	artist.text = nowPlaying.artist
+	
+	
+func _on_Previous_activated(_button):
+	nowPlayingIndex -= 1
+	nowPlaying = nowPlayingList[nowPlayingIndex % nowPlayingList.size()]
+	updateNowPlaying()
+
+
+func _on_Next_activated(_button):
+	nowPlayingIndex += 1	
+	nowPlaying = nowPlayingList[nowPlayingIndex % nowPlayingList.size()]
+	updateNowPlaying()
+
+
+func _on_Rewind_activated(_button):
+	elapsed -= 10
+	if elapsed < 0:
+		elapsed = 0
+	update_progress()
+
+
+func _on_Pause_activated(_button):
+	paused = !paused
+	if paused:
+		pauseButton.text = "Resume"
+	else:
+		pauseButton.text = "Pause"
+
+
+func _on_Forward_activated(_button):
+	elapsed += 10
+	if elapsed > nowPlayingLength:
+		_on_Next_activated(null)
+	update_progress()
--- src/Scripts/Center Dashboard/Radio_Menu.gd
+++ src/Scripts/Center Dashboard/Radio_Menu.gd
@@ -3,8 +3,22 @@ extends Node2D
 
 var station = 88.1
 
+var mode = "fm"
+var fmPresets = [
+	"92.3",
+	"96.5",
+	"103.5",
+]
+
+var amPresets = [
+	"790",
+	"950",
+	"1340",
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_on_FM_activated(null)
 	updateStationText()
 
 
@@ -26,31 +40,60 @@ func _ready():
 
 
 func _on_Radio_Control_rotation_increase():
-	station += 0.2
+	if (mode == "fm"):
+		station += 0.2
+	else:
+		station += 10
 	updateStationText()
 
 
 func _on_Radio_Control_rotation_decrease():
-	station -= 0.2
+	if (mode == "fm"):
+		station -= 0.2
+	else:
+		station -= 10
 	updateStationText()
 	
 func updateStationText():
-	if (station > 107.9):
-		station = 88.1
-	if (station < 88.1):
-		station = 107.9
+	if (mode == "fm"):
+		if (station > 107.9):
+			station = 88.1
+		if (station < 88.1):
+			station = 107.9
+	else:
+		if (station > 1700):
+			station = 540
+		if (station < 540):
+			station = 1700
 
 	$CenterContainer/Station.text = str(station)
 
 
 func _on_preset_activated(button):
-	station = round(float(button.label_text) * 10)/10
+	if (mode == "fm"):
+		station = round(float(button.label_text) * 10)/10
+	else:
+		station = int(button.label_text)
 	updateStationText()
 
 
-func _on_AM_activated():
-	pass # Replace with function body.
+func _on_AM_activated(_button):
+	mode = "am"
+	station = 540
+	$Preset1.label_text = amPresets[0]
+	$Preset2.label_text = amPresets[1]
+	$Preset3.label_text = amPresets[2]
+	updateStationText()
 
 
-func _on_FM_activated():
+func _on_FM_activated(_button):
+	mode = "fm"
+	station = 88.1
+	$Preset1.label_text = fmPresets[0]
+	$Preset2.label_text = fmPresets[1]
+	$Preset3.label_text = fmPresets[2]
+	updateStationText()
+
+
+func _on_Previous_activated():
 	pass # Replace with function body.
