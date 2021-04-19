2a731961975661828d0035ee965d4efb8864dd72
Maureen Gloria <maureenmgloria@gmail.com>
2021-04-18 21:41:17 -0500

    Adjust gauges and fuel

--- src/Scripts/Main Dashboard/Alerts.gd
+++ src/Scripts/Main Dashboard/Alerts.gd
@@ -7,6 +7,9 @@ extends Node2D
 
 signal tire_pressure_toggled
 
+var normal = load("res://Sprites/Center Dashboard/tempnormal.png") 
+var hot = load("res://Sprites/Center Dashboard/temphot.png") 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -28,9 +31,9 @@ func _on_Show_Alerts_pressed():
 
 func _on_Tire_Pressure_toggled(button_pressed):
 	if(button_pressed):
-		$"tire-pressure".visible = false
-	else:
 		$"tire-pressure".visible = true
+	else:
+		$"tire-pressure".visible = false
 	
 	emit_signal('tire_pressure_toggled')
 	# pass # Replace with function body.
@@ -39,10 +42,19 @@ func _on_Tire_Pressure_toggled(button_pressed):
 func _on_Show_Alerts_toggled(button_pressed):
 	if(button_pressed):
 		for n in self.get_children():
-			n.visible = false
+			n.visible = true
 	else:
 		for n in self.get_children():
-			n.visible = true
+			n.visible = false
 		$"../Control/Tire Pressure".pressed = false
 			#$"../Control/Tire Pressure"._toggled = false
 		
+
+
+func _on_Set_Hot_toggled(button_pressed):
+	if(button_pressed):
+		$"engine-temp".visible = true
+		$"../placeholderrpm/placeholdertemp".texture = hot
+	else:
+		$"engine-temp".visible = false
+		$"../placeholderrpm/placeholdertemp".texture = normal
